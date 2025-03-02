772f1b78ad6d33099105469d9a38534c612062ec
necoarcc <99202510+necoarcc@users.noreply.github.com>
2025-03-02 15:22:12 +0700

    project files

---
+++ discord-custom-rpc/main.cpp
@@ -0,0 +1,9 @@
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <Windows.h>
+
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+{
+	return 0;
+}
